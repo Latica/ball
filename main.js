2ec3e64804986c56769fbd75c17ec47d32dc1110
Latica <laticaitm@gmail.com>
2017-02-03 18:13:13 +0100

    First commit

---
+++ main.js
@@ -0,0 +1,18 @@
+var ball = document.getElementById('ball');
+
+addEventListener('keydown',pomicanje);
+
+function pomicanje (e){
+   if (e.which == 37) {
+       ball.style.left = '50px';
+   }
+   if (e.which == 38) {
+       ball.style.top = '50px';
+   }
+   if (e.which == 39) {
+       ball.style.left = '1100px';
+   }
+   if (e.which == 40) {
+       ball.style.top = '600px';
+   }
+   }
